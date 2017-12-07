a231ec708b399051081babe58aad025322294c03
徐玥 <xuyue_ustb@163.com>
2017-12-07 15:24:02 +0800

    v4.0

---
+++ src/api/config.js
@@ -0,0 +1,13 @@
+export const commonParams = {
+  g_tk: 5381,
+  inCharset: 'utf_8',
+  outCharset: 'utf_8',
+  notice: 0,
+  format: 'jsonp'
+}
+
+export const options = {
+  param: 'jsonpCallback'
+}
+
+export const ERR_OK = 0
---
+++ src/api/recommend.js
@@ -0,0 +1,31 @@
+import jsonp from 'common/js/jsonp'
+import { commonParams, options } from './config'
+export function getRecommend() {
+  let url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
+
+  const data = Object.assign({}, commonParams, {
+    platform: 'h5',
+    uin: 0,
+    needNewCode: 1
+  })
+
+  return jsonp(url, data, options)
+}
+
+export function getDiscList() {
+  let url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg'
+
+  const data = Object.assign({}, commonParams, {
+    platform: 'yqq',
+    hostUin: 0,
+    uin: 0,
+    sin: 0,
+    ein: 29,
+    sortId: 5,
+    categoryId: 10000000,
+    needNewCode: 0,
+    rnd: Math.random()
+  })
+
+  return jsonp(url, data, options)
+}
